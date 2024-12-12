4a2e36576de0f2a9ced53356e4061b0242657b20
Nasaro <nasrallah.elkaboussi@gmail.com>
2024-12-12 13:47:05 +0100

    2024-12-12

--- juego.php
+++ juego.php
@@ -9,6 +9,29 @@
 </head>
 <body>
     <?php
+        //explode
+        //implode
+        
+
+         function mostrarFormulario($pregunta, $idPreg, $nombreEnv,$cont) {
+            echo $cont;
+            echo '<div class="container">
+                    <div class="wrapper">
+                    ' . $cont . '
+                        <form action="#" method="post" id="registro">
+                            <label for="respuesta">' . explode(",", $pregunta)[0] . '</label><br>
+                            <input type="text" placeholder="respuesta" name="rsp">
+                            <input type="hidden" name="pregunta" value="' . $pregunta . '">
+                            <input type="hidden" name="idPreg" value="' . $idPreg . '">
+                            <input type="hidden" name="cont" value="' . $cont . '">
+                            <input type="submit" class="btn" name="' . $nombreEnv . '" value="Responder">
+                        </form>
+                    </div>
+                </div>';
+        }
+
+
+
         require_once 'usuarios.php';     
         require_once "preguntas.php";
         $db = new mysqli('localhost', 'root', '', 'cuestionario');
@@ -17,6 +40,7 @@
         $arraypreg = $pg->getDatos() ;
         $numpreg = [];
         $cont=0;
+        $comprobar=false;
         for ($i = 0; $i < 5; $i++) {
             do {
                 $numAleatorio = mt_rand(0, 9);
@@ -25,46 +49,59 @@
         }
 
         
-        if (isset($_POST["env"])){
-            $pregunta= explode(",",$_POST["pregunta"]);
-            echo $pregunta[0];
-            echo $pregunta[1];
-            echo "id preg: ".$_POST["idPreg"];
-            $comprobar = $pg->comprobar($_POST["rsp"],$pregunta[1]);
-            
-            if($comprobar==false) {
-                
-            }
-            if ($comprobar==true) {
-                
-            }
-        }else {
-            echo '<div class="container">
-                <div class="wrapper">
-                    <form action="" method="post" id="registro">
-                        <label for="">'. explode(",",$arraypreg[$numpreg[$cont]])[0] . '<label><br>
-                        <input type="text" placeholder="respuesta" name="rsp">
-                        <input type="submit" class="btn" name="env" value="Responder">
-                        <input type="hidden" name="pregunta" value="' .$arraypreg[$numpreg[$cont]]. '">
-                        <input type="hidden" name="idPreg" value="' .$numpreg[$cont]+1.. '">
-                    <form>
-                </div>
-             </div>';
-        }
 
-        function generarPreg($cont,$arraypreg,$numpreg){
-            echo '<div class="container">
-                <div class="wrapper">
-                    <form action="" method="post" id="registro">
-                        <label for="">'. explode(",",$arraypreg[$numpreg[$cont]])[0] . '<label><br>
-                        <input type="text" placeholder="respuesta" name="rsp">
-                        <input type="submit" class="btn" name="env" value="Responder">
-                        <input type="hidden" name="pregunta" value="' .$arraypreg[$numpreg[$cont]]. '">
-                        <input type="hidden" name="idPreg" value="' .$numpreg[$cont]+1.. '">
-                    <form>
-                </div>
-             </div>';
+
+        if (isset($_POST["env"])) {
+            $cont=$_POST["cont"];
+            if($cont < 5){
+                $pregunta = explode(",", $_POST["pregunta"]);
+                $comprobar = $pg->comprobar($_POST["rsp"], $pregunta[1]);
+                
+
+                if ($comprobar == false) {
+                    mostrarFormulario($_POST["pregunta"], $_POST["idPreg"], "env",$cont);
+                }else{
+                    $cont++;
+                    mostrarFormulario($arraypreg[$numpreg[$cont]], $numpreg[$cont], "env",$cont);
+                }
+            }else{
+
+            }
+
+            
+        } else {
+            mostrarFormulario($arraypreg[$numpreg[$cont]], $numpreg[$cont], "env",$cont);
         }
+        /*
+        $cont++;
+        if (isset($_POST["env2"])) {
+            $pregunta = explode(",", $_POST["pregunta"]);
+            $comprobar = $pg->comprobar($_POST["rsp"], $pregunta[1]);
+
+            if ($comprobar == false) {
+                mostrarFormulario($_POST["pregunta"], $_POST["idPreg"], "env2",$cont);
+            }
+        } else {
+            
+            mostrarFormulario($arraypreg[$numpreg[$cont]], $numpreg[$cont], "env2",$cont);
+           
+        }
+        $cont++;
+        if (isset($_POST["env3"])) {
+            $pregunta = explode(",", $_POST["pregunta"]);
+            $comprobar = $pg->comprobar($_POST["rsp"], $pregunta[1]);
+
+            if ($comprobar == false) {
+                mostrarFormulario($_POST["pregunta"], $_POST["idPreg"], "env3",$cont);
+            }
+        } else {
+            
+            mostrarFormulario($arraypreg[$numpreg[$cont]], $numpreg[$cont], "env3",$cont);
+            
+        }
+        $cont++;*/
+       
+
 
         
     ?>
